6174fa4a7c890d71300b230f02895d61984b50c9
Davi <daviibrito111@gmail.com>
2023-05-11 11:33:31 -0300

    Get GameDTO by id

--- src/main/java/com/daviembrito/dslist/controllers/GameController.java
+++ src/main/java/com/daviembrito/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.daviembrito.dslist.controllers;
 
+import com.daviembrito.dslist.dto.GameDTO;
 import com.daviembrito.dslist.dto.GameShortDTO;
 import com.daviembrito.dslist.entities.Game;
 import com.daviembrito.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,13 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findGameDTO(@PathVariable Long id) {
+        return gameService.findGameById(id);
+    }
+
     @GetMapping
-    public List<GameShortDTO> findAllShort() {
+    public List<GameShortDTO> findAllGameShortDTO() {
         return gameService.findAllGameShort();
     }
 }
