d24bec93390b51408064eff1129fb89dcc9bb8b2
Davi <daviibrito111@gmail.com>
2023-05-11 11:33:59 -0300

    Changed transactions to readOnly

--- src/main/java/com/daviembrito/dslist/services/GameService.java
+++ src/main/java/com/daviembrito/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.daviembrito.dslist.services;
 
+import com.daviembrito.dslist.dto.GameDTO;
 import com.daviembrito.dslist.dto.GameShortDTO;
 import com.daviembrito.dslist.entities.Game;
 import com.daviembrito.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findGameById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameShortDTO> findAllGameShort() {
         return convertGameToDTO(gameRepository.findAll());
     }
