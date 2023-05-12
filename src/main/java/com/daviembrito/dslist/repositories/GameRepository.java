4ac91d4c363a3bc806f1c7e9416c3edb37993628
Davi <daviibrito111@gmail.com>
2023-05-12 00:11:17 -0300

    Removed blank space

--- src/main/java/com/daviembrito/dslist/repositories/GameRepository.java
+++ src/main/java/com/daviembrito/dslist/repositories/GameRepository.java
@@ -3,6 +3,4 @@ package com.daviembrito.dslist.repositories;
 import com.daviembrito.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
+public interface GameRepository extends JpaRepository<Game, Long> {}
