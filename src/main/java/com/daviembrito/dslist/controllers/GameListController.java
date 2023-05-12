28e1161fabaccb09a782b7b2264cd2a47443a4c5
Davi <daviibrito111@gmail.com>
2023-05-12 02:44:20 -0300

    Added endpoint /lists/{id}/games structure

--- src/main/java/com/daviembrito/dslist/controllers/GameListController.java
+++ src/main/java/com/daviembrito/dslist/controllers/GameListController.java
@@ -1,7 +1,9 @@
 package com.daviembrito.dslist.controllers;
 
 import com.daviembrito.dslist.dto.GameListDTO;
+import com.daviembrito.dslist.dto.GameShortDTO;
 import com.daviembrito.dslist.services.GameListService;
+import com.daviembrito.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +18,16 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAllGameListDTO() {
         return gameListService.findAllGameList();
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameShortDTO> findGamesByList(@PathVariable Long listId) {
+        return gameService.findGamesByList(listId);
     }
 }
--- src/main/java/com/daviembrito/dslist/services/GameService.java
+++ src/main/java/com/daviembrito/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.daviembrito.dslist.services;
 import com.daviembrito.dslist.dto.GameDTO;
 import com.daviembrito.dslist.dto.GameShortDTO;
 import com.daviembrito.dslist.entities.Game;
+import com.daviembrito.dslist.projections.GameShortProjection;
 import com.daviembrito.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,17 +17,26 @@ public class GameService {
     private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
-    public GameDTO findGameById(Long id) {
-        Game result = gameRepository.findById(id).get();
+    public List<GameShortDTO> findGamesByList(Long listId) {
+        return convertProjectionsToGameShortDTO(gameRepository.searchByList(listId));
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findGameById(Long gameId) {
+        Game result = gameRepository.findById(gameId).get();
         return new GameDTO(result);
     }
 
     @Transactional(readOnly = true)
     public List<GameShortDTO> findAllGameShort() {
-        return convertGameToDTO(gameRepository.findAll());
+        return convertGamesToGameShortDTO(gameRepository.findAll());
     }
 
-    private List<GameShortDTO> convertGameToDTO(List<Game> games) {
+    private List<GameShortDTO> convertGamesToGameShortDTO(List<Game> games) {
         return games.stream().map(GameShortDTO::new).toList();
+    }
+
+    private List<GameShortDTO> convertProjectionsToGameShortDTO(List<GameShortProjection> gameProjections) {
+        return gameProjections.stream().map(GameShortDTO::new).toList();
     }
 }
